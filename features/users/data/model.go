eab0e13e6d9eef1f66f300611869f6c69975b349
kgriffinh <griffinh29@gmail.com>
2023-01-16 21:52:26 +0800

    add entity, model, query

---
+++ features/users/data/model.go
@@ -0,0 +1,33 @@
+package data
+
+import (
+	// "socialmedia/features/posts/data"
+	"socialmedia/features/users"
+
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	Username string
+	Email    string
+	Password string
+}
+
+func ToCore(data User) users.Core {
+	return users.Core{
+		ID:       data.ID,
+		Username: data.Username,
+		Email:    data.Email,
+		Password: data.Password,
+	}
+}
+
+func CoreToData(data users.Core) User {
+	return User{
+		Model:    gorm.Model{ID: data.ID},
+		Username: data.Username,
+		Email:    data.Email,
+		Password: data.Password,
+	}
+}

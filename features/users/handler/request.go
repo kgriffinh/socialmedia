d4c4da07ae3acdadeecbbb9617f236d9ae38ce76
kgriffinh <griffinh29@gmail.com>
2023-01-16 22:11:44 +0800

    fix email parameter

---
+++ features/users/handler/request.go
@@ -0,0 +1,43 @@
+package handler
+
+import "socialmedia/features/users"
+
+type LoginRequest struct {
+	Username string `json:"username" form:"username"`
+	Password string `json:"password" form:"password"`
+}
+
+type RegisterRequest struct {
+	Username string `json:"username" form:"username"`
+	Email    string `json:"email" form:"email"`
+	Password string `json:"password" form:"password"`
+}
+
+type UpdateRequest struct {
+	Username string `json:"username" form:"username"`
+	Email    string `json:"email" form:"email"`
+}
+
+func ReqToCore(data interface{}) *users.Core {
+	res := users.Core{}
+
+	switch data.(type) {
+	case LoginRequest:
+		cnv := data.(LoginRequest)
+		res.Username = cnv.Username
+		res.Password = cnv.Password
+	case RegisterRequest:
+		cnv := data.(RegisterRequest)
+		res.Email = cnv.Email
+		res.Username = cnv.Username
+		res.Password = cnv.Password
+	case UpdateRequest:
+		cnv := data.(UpdateRequest)
+		res.Email = cnv.Email
+		res.Username = cnv.Username
+	default:
+		return nil
+	}
+
+	return &res
+}

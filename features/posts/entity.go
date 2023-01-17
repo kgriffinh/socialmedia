6c9d1e5a137412223769d038ffd685fcf602428e
audizzy <zulfeekei@gmail.com>
2023-01-17 09:36:10 +0700

    added entityy posts

--- features/posts/entity.go
+++ features/posts/entity.go
@@ -1,1 +1,30 @@
 package posts
+
+import "github.com/labstack/echo/v4"
+
+type Core struct {
+	ID          uint
+	content     string
+	img_content string
+}
+
+type PostHandler interface {
+	Add() echo.HandlerFunc
+	GetPost() echo.HandlerFunc
+	Update() echo.HandlerFunc
+	Delete() echo.HandlerFunc
+}
+
+type PostService interface {
+	Add(token interface{}, newPost Core) (Core, error)
+	GetPost() ([]Core, error)
+	Update(token interface{}, postID int, updateData Core) (Core, error)
+	Delete(token interface{}, postID int) error
+}
+
+type PostData interface {
+	Add(userID int, newPost Core) (Core, error)
+	GetPost() ([]Core, error)
+	Update(postID int, updateData Core) (Core, error)
+	Delete(postID int, userID int) error
+}

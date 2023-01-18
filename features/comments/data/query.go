c6279ce07e0dff85c654a03bd9279aa75d953757
kgriffinh <griffinh29@gmail.com>
2023-01-18 12:21:10 +0800

    add entity, model, query, service in features comment

---
+++ features/comments/data/query.go
@@ -0,0 +1,93 @@
+package data
+
+import (
+	"errors"
+	"log"
+	"socialmedia/features/comments"
+
+	"gorm.io/gorm"
+)
+
+type commentData struct {
+	db *gorm.DB
+}
+
+func New(db *gorm.DB) comments.CommentData {
+	return &commentData{
+		db: db,
+	}
+}
+
+func (cd *commentData) Add(postID int, newComment comments.Core) (comments.Core, error) {
+	cnv := CoreToData(newComment)
+	cnv.PostID = uint(postID)
+	err := cd.db.Create(&cnv).Error
+	if err != nil {
+		log.Println("query error", err.Error())
+		return comments.Core{}, errors.New("querry error, fail to add item")
+	}
+
+	newComment.ID = cnv.ID
+	return newComment, nil
+}
+
+func (cd *commentData) GetComment() ([]comments.Core, error) {
+	res := []Comments{}
+	err := cd.db.Find(&res).Error
+	if err != nil {
+		log.Println("no data found")
+		return []comments.Core{}, errors.New("data not found")
+	}
+
+	result := []comments.Core{}
+	for i := 0; i < len(res); i++ {
+		temp := res[i]
+		result = append(result, ToCore(temp))
+		qry := User{}
+		err := cd.db.Where("id = ?", res[i].UserID).First(&qry).Error
+		if err != nil {
+			log.Println("no data found")
+			return []comments.Core{}, errors.New("data not found")
+		}
+		result[i].Owner = qry.Username
+	}
+
+	return result, nil
+}
+
+func (cd *commentData) Update(commentID int, postID int, updateData comments.Core) (comments.Core, error) {
+	cnv := CoreToData(updateData)
+	qry := cd.db.Model(&Comments{}).Where("id = ? AND post_id = ?", commentID, postID).Updates(&cnv)
+
+	affrows := qry.RowsAffected
+	if affrows == 0 {
+		log.Println("no rows affected")
+		return comments.Core{}, errors.New("tidak ada comment yang terhapus")
+	}
+
+	err := qry.Error
+	if err != nil {
+		log.Println("update comment query error", err.Error())
+		return comments.Core{}, err
+	}
+
+	return ToCore(cnv), nil
+}
+
+func (cd *commentData) Delete(commentID int, postID int) error {
+	qry := cd.db.Where("id = ? AND post_id = ?", commentID, postID).Delete(&Comments{})
+	err := qry.Error
+
+	affrows := qry.RowsAffected
+	if affrows == 0 {
+		log.Println("no rows affected")
+		return errors.New("tidak ada post yang terhapus")
+	}
+
+	if err != nil {
+		log.Println("delete query error")
+		return errors.New("tidak bisa menghapus data")
+	}
+
+	return nil
+}
--- features/comments/entity.go
+++ features/comments/entity.go
@@ -1,1 +1,30 @@
 package comments
+
+import "github.com/labstack/echo"
+
+type Core struct {
+	ID    uint
+	Text  string
+	Owner string
+}
+
+type CommentHandler interface {
+	Add() echo.HandlerFunc
+	GetComment() echo.HandlerFunc
+	Update() echo.HandlerFunc
+	Delete() echo.HandlerFunc
+}
+
+type CommentService interface {
+	Add(token interface{}, newComment Core) (Core, error)
+	GetComment() ([]Core, error)
+	Update(token interface{}, commentID int, updateData Core) (Core, error)
+	Delete(token interface{}, commentID int) error
+}
+
+type CommentData interface {
+	Add(postID int, newPost Core) (Core, error)
+	GetComment() ([]Core, error)
+	Update(commentID int, postID int, updateData Core) (Core, error)
+	Delete(commentID int, postID int) error
+}
